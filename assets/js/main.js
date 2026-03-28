a905a48c49abace55d018beaa047692ada007c9f
Ken Starzer <kenstarzer@users.noreply.github.com>
2026-03-28 10:11:41 -0500

    udpated js and css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 // main.js
+/*
 let rotation = 0;
 document.addEventListener("click", function () {
 let min = 10;
@@ -6,3 +7,6 @@ let max = 40;
 rotation += Math.floor(Math.random() * (max - min + 1)) + min;
 document.body.style.transform = 'rotate(${rotation}deg)';
 });
+*/
+
+document.body.style.transform = 'rotate(90deg)';
